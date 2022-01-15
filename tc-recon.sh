da888703df915026385180b4e562b26e75f28e98
DFIR-Life <ksahota@604security.com>
2022-01-14 22:56:21 -0500

    fixed path issue

--- tc-recon.sh
+++ tc-recon.sh
@@ -2,7 +2,7 @@
 
 #setup
 DOMAIN=$1
-DIRECTORY=./tc-recon-out
+DIRECTORY=~/tc-recon-out
 
 
 TODAY=$(date) 
@@ -17,7 +17,7 @@ case $2 in
 	echo "The results of nmap scan are stored in $DIRECTORY/nmap."
 	;;
 	dir)
-	dirsearch -u $DOMAIN  -e php -o /home/kali/scripts/$DIRECTORY/${DOMAIN}_dirsearch --format=plain
+	dirsearch -u $DOMAIN  -e php -o $DIRECTORY/${DOMAIN}_dirsearch --format=plain
 	echo "The results of dirsearch scan are stored in ${DIRECTORY}_dirsearch."
 	;;
 	crt)
@@ -25,21 +25,21 @@ case $2 in
 	echo "The results of cert parsing is stored in $DIRECTORY/${DOMAIN}_crt."
 	;;
 	who)
-    whois $DOMAIN  >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_misc
-	nslookup $DOMAIN  >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_misc
-	wget $DOMAIN/robots.txt --no-check-certificate   >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_robots
+    whois $DOMAIN  >>  $DIRECTORY/${DOMAIN}_misc
+	nslookup $DOMAIN  >>  $DIRECTORY/${DOMAIN}_misc
+	wget $DOMAIN/robots.txt --no-check-certificate   >>  $DIRECTORY/${DOMAIN}_robots
 	echo "The results of misc tools are stored in $DIRECTORY/"
 	;;
 	*)
 	nmap $DOMAIN -A -oA $DIRECTORY/${DOMAIN}_nmap 
     echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-	dirsearch -u $DOMAIN  -e php -o /home/kali/scripts/$DIRECTORY/${DOMAIN}_dirsearch --format=plain
+	dirsearch -u $DOMAIN  -e php -o $DIRECTORY/${DOMAIN}_dirsearch --format=plain
     echo "The results of dirsearch scan are stored in ${DIRECTORY}_dirsearch."
 	curl "https://crt.sh/?q=$DOMAIN&output=json" -o $DIRECTORY/${DOMAIN}_crt 
     echo "The results of cert parsing is stored in $DIRECTORY/${DOMAIN}_crt."
-	whois $DOMAIN  >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_misc
-	nslookup $DOMAIN  >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_misc
-    wget $DOMAIN/robots.txt --no-check-certificate   >>  /home/kali/scripts/$DIRECTORY/${DOMAIN}_robots
+	whois $DOMAIN  >>  $DIRECTORY/${DOMAIN}_misc
+	nslookup $DOMAIN  >>  $DIRECTORY/${DOMAIN}_misc
+    wget $DOMAIN/robots.txt --no-check-certificate   >>  $DIRECTORY/${DOMAIN}_robots
     echo "The results of misc tools are stored in $DIRECTORY/"
 	;;
 esac
